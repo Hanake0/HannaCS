09d2a4786d57ee95e12f6dd3fa9ec0e64f2f4719
Breno Gomes Haese <Bghaesee@outlook.com>
2021-01-12 23:17:01 -0200

    Push inicial

---
+++ Hanna.cs
@@ -0,0 +1,16 @@
+﻿using DSharpPlus;
+using System.Threading.Tasks;
+
+namespace Hanna
+{
+    public class Bot
+    {
+        public DiscordClient Client = { get; private set; }
+
+        public async Task RunAsyn()
+        {
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hanna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot Hanna = new Bot();
+            Hanna.RunAsyn().GetAwaiter().ConfigAwait(false);
+        }
+    }
+}
