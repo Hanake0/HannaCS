fec0be4868e2539967d16562c474937a9f4c866a
Hanake0 <bghaesee@outlook.com>
2021-07-13 17:03:59 -0300

    Fixed typos and updated dependencies

--- mongodb/manager.cs
+++ mongodb/manager.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MongoDB.Driver;
+
 namespace Hanna.MongoDB
 {
 	public class DBManager
 	{
 		public MongoClient Client { get; private set; }
 
-		public DbManager()
+		public DBManager()
 		{
 
 			var client = new MongoClient("mongodb+srv://Hanna:<password>@cluster0.ekjrf.mongodb.net/<dbname>?retryWrites=true&w=majority");
